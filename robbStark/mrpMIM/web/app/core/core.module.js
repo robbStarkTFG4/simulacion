334f4de2b00dd2bcbb162a22c81addd0bb56093d
robbStarkTFG4 <robbstarktfg@gmail.com>
2015-03-19 23:00:21 -0800

    template

---
+++ robbStark/mrpMIM/web/app/core/core.module.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    angular.module('app.core', [
+        /*
+         * Angular modules
+         */
+        //'ngAnimate', 'ngRoute', 'ngSanitize',
+        'ui.router'
+        /*
+         * Our reusable cross app code modules
+         */
+        //'blocks.exception', 'blocks.logger', 'blocks.router',
+        /*
+         * 3rd Party modules
+         */
+        //'ngplus'
+    ]);
+})();
---
+++ robbStark/mrpMIM/web/app/prueba/prueba.config.route.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+    var pr = angular
+            .module('app.prueba');
+   // pr.config(rutas);
+   
+
+})();
